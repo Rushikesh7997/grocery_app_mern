2bf7add95c41272f7c1a5e34d662fb10d72abb56
Rushikesh <pardhikar26@gmail.com>
2025-06-22 20:12:39 +0530

    context setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,23 @@
+import {Routes, Route, BrowserRouter} from "react-router-dom"
+import { Home } from "./pages/Home";
+import { Products } from "./pages/Products";
+import { ProductDetails } from "./pages/ProductDetails";
+import { Cart } from "./pages/Cart";
+
 function App() {
   return (
     <>
       <div>
-        <h1 className="text-red-400 ">hello</h1>
+        <div>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/products" element={<Products/>}/>
+              <Route path="/products/:id" element={<ProductDetails/>}/>
+              <Route path="/cart" element={<Cart/>}/>
+            </Routes>
+          </BrowserRouter>
+        </div>
       </div>
     </>
   );
---
+++ client/src/context/AppContext.jsx
@@ -0,0 +1,16 @@
+import React, { createContext } from 'react'
+import { useNavigate } from 'react-router-dom';
+
+export const AppContext = createContext(null)
+
+const AppContextProvider = ({children}) =>{
+    const navigate = useNavigate()
+    const value = {navigate};
+    return (
+        <AppContext.Provider value={value}>
+            {children}
+        </AppContext.Provider>
+    )
+}
+
+export default AppContextProvider;
