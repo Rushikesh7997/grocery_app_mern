19e8705e064851e5142e32c1fa4bcbdb07eeb44b
Rushikesh <pardhikar26@gmail.com>
2025-06-24 21:12:01 +0530

    work on auth

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,29 @@
-import {Routes, Route, BrowserRouter} from "react-router-dom"
+import {Routes, Route, BrowserRouter, useLocation} from "react-router-dom"
 import { Home } from "./pages/Home";
 import { Products } from "./pages/Products";
 import { ProductDetails } from "./pages/ProductDetails";
 import { Cart } from "./pages/Cart";
+import { Navbar } from "./components/Navbar";
+import { useContext } from "react";
+import { AppContext } from "./context/AppContext";
+import { MyOrders } from "./pages/MyOrders";
 
 function App() {
+  const {isSeller} = useContext(AppContext);
+  const isSellerPath = useLocation().pathname.includes("seller")
   return (
-    <>
       <div>
+        {isSellerPath ? null : <Navbar/>}
         <div>
-          <BrowserRouter>
             <Routes>
               <Route path="/" element={<Home/>}/>
               <Route path="/products" element={<Products/>}/>
               <Route path="/products/:id" element={<ProductDetails/>}/>
               <Route path="/cart" element={<Cart/>}/>
+              <Route path="/my-orders" element={<MyOrders/>}/>
             </Routes>
-          </BrowserRouter>
         </div>
       </div>
-    </>
   );
 }
 
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,16 +1,23 @@
-import React, { createContext } from 'react'
-import { useNavigate } from 'react-router-dom';
+import React, { createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-export const AppContext = createContext(null)
+export const AppContext = createContext(null);
 
-const AppContextProvider = ({children}) =>{
-    const navigate = useNavigate()
-    const value = {navigate};
-    return (
-        <AppContext.Provider value={value}>
-            {children}
-        </AppContext.Provider>
-    )
-}
+const AppContextProvider = ({ children }) => {
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [isSeller, setIsSeller] = useState(null);
+  const [showUserLogin, setShowUserLogin] = useState(false);
+  const value = {
+    navigate,
+    user,
+    setUser,
+    isSeller,
+    setIsSeller,
+    showUserLogin,
+    setShowUserLogin,
+  };
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
+};
 
 export default AppContextProvider;
